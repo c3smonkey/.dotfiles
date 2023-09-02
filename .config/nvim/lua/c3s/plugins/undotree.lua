45cb6aa4cade084f72c63e2c72918f86d8657fa7
c3smonkey <c3smonkey@gmail.com>
2023-09-02 07:54:11 +0200

    chore: [nvim] add undotree

---
+++ .config/nvim/lua/c3s/plugins/undotree.lua
@@ -0,0 +1,12 @@
+return {
+  "mbbill/undotree",
+
+  config = function()
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>uh", "<cmd>UndotreeToggle<cr>", { desc = "Undotree visualizes the undo history" })
+
+    print("Undotree keymap set")
+  end,
+}
