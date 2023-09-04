63da97e569ab1758968d77b660da675b79c51457
Marcel Widmer <marcel.widmer@helsana.ch>
2023-09-04 22:33:54 +0200

    chore: [nvim] change nvim tabs and shiftwith to 2

--- .config/nvim/lua/c3s/core/options.lua
+++ .config/nvim/lua/c3s/core/options.lua
@@ -5,8 +5,8 @@ opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 2 --  spaces for tabs (prettier default)
+opt.shiftwidth = 4 --  spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
@@ -19,6 +19,7 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
+opt.scrolloff = 10
 
 -- appearance
 
