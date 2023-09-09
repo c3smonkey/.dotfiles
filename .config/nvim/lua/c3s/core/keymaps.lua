58a7037aeefb10d7e8877cee45cc4805835cdb11
c3smonkey <c3smonkey@gmail.com>
2023-09-09 21:40:24 +0200

    chore: [nvim] change ESC mapping from jk to jkj

--- .config/nvim/lua/c3s/core/keymaps.lua
+++ .config/nvim/lua/c3s/core/keymaps.lua
@@ -6,8 +6,8 @@ local keymap = vim.keymap -- for conciseness
 ---------------------
 -- General Keymaps -------------------
 
--- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+-- use jkj to exit insert mode
+keymap.set("i", "jkj", "<ESC>", { desc = "Exit insert mode with jkj" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
