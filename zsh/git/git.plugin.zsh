58b6bf246c49a1bce42ada90a8bc26bf21f8be76
Marcel Widmer <marcel.widmer@helsana.ch>
2024-02-25 17:50:44 +0100

    fix: company setup

--- zsh/git/git.plugin.zsh
+++ zsh/git/git.plugin.zsh
@@ -1,7 +1,8 @@
 # Git version checking
 autoload -Uz is-at-least
 git_version="${${(As: :)$(git version 2>/dev/null)}[3]}"
-
+autoload -Uz compinit
+compinit
 #
 # Functions Current
 # (sorted alphabetically by function name)
