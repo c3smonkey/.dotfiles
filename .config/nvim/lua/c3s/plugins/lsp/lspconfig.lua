425ec75979e449309986fe87119f743e3632cec7
c3smonkey <c3smonkey@gmail.com>
2024-03-10 10:06:51 +0100

    chore: [nvim] configure kotlin LSP

--- .config/nvim/lua/c3s/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/c3s/plugins/lsp/lspconfig.lua
@@ -25,58 +25,11 @@ return {
     local on_attach = function(client, bufnr)
       -- keybind options
       local opts = { noremap = true, silent = true, buffer = bufnr }
-
       -- set keybinds
       opts.desc = "Show LSP references"
       keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
       opts.desc = "Go to declaration"
       keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-      opts.desc = "Show LSP implementations"
-      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-      opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-      opts.desc = "Smart rename"
-      keymap.set("n", "<leader>rn", ":IncRename ", opts) -- smart rename
-
-      opts.desc = "Show buffer diagnostics"
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-      opts.desc = "Show line diagnostics"
-      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-      opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-
-      -- typescript specific keymaps (e.g. rename file and update imports)
-      if client.name == "tsserver" then
-        opts.desc = "Rename file and update file imports"
-        keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-
-        opts.desc = "Rename file and update file imports"
-        keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>", opts) -- organize imports (not in youtube nvim video)
-
-        opts.desc = "Remove unused imports"
-        keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>", opts) -- remove unused variables (not in youtube nvim video)
-      end
     end
 
     -- used to enable autocompletion (assign to every lsp server config)
@@ -142,12 +95,24 @@ return {
       filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
     })
 
-    -- configure kotlin language server
+    -- Configure the Kotlin language server
     lspconfig.kotlin_language_server.setup({
-      cmd = { "kotlin-language-server" }, -- replace with path to your kotlin-language-server if necessary
+      cmd = { "kotlin-language-server" }, -- Replace with the actual path to your kotlin-language-server executable
       root_dir = lspconfig.util.root_pattern("pom.xml", "settings.gradle", "settings.gradle.kts"),
       capabilities = capabilities,
-      on_attach = on_attach,
+      on_attach = function(client, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+
+        -- Set keybinds
+        opts.desc = "Show LSP references"
+        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- Show definition, references
+
+        opts.desc = "Go to declaration"
+        keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- Go to declaration
+
+        -- Add gd keybinding for definition
+        keymap.set("n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts) -- Go to definition
+      end,
     })
 
     -- configure lua server (with special settings)
