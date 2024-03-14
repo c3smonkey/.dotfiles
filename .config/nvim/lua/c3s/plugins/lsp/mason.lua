237a1bdd034524fad1b6c8d8b0cf9606ef918b40
Marcel Widmer <marcel.widmer@helsana.ch>
2024-03-14 16:14:47 +0100

    chore: [nvim] configure kotlin_language_server in mason.lua

--- .config/nvim/lua/c3s/plugins/lsp/mason.lua
+++ .config/nvim/lua/c3s/plugins/lsp/mason.lua
@@ -30,6 +30,7 @@ return {
         "emmet_ls",
         "prismals",
         "yamlls",
+        "kotlin_language_server",
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
