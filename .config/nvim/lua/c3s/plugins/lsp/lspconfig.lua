e8fd442d50a9754b9aadd9b3ba6b31c02a512c3e
c3smonkey <c3smonkey@gmail.com>
2024-03-15 20:56:17 +0100

    chore: [nvim] remove some LSPs

--- .config/nvim/lua/c3s/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/c3s/plugins/lsp/lspconfig.lua
@@ -73,19 +73,6 @@ return {
       on_attach = on_attach,
     })
 
-    -- configure prisma orm server
-    lspconfig["prismals"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
-    -- configure graphql language server
-    lspconfig["graphql"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-    })
-
     -- configure emmet language server
     lspconfig["emmet_ls"].setup({
       capabilities = capabilities,
