e0f1ae74f7272dfbde8870e443b47430160c0568
c3smonkey <c3smonkey@gmail.com>
2024-03-24 00:08:12 +0100

    wip

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,21 +1,21 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
 
-  config = function()
-    vim.keymap.set("n", "<leader>ee", function()
-      if neotree_open then
-        vim.cmd(":Neotree close")
-        neotree_open = false
-      else
-        vim.cmd(":Neotree filesystem reveal left")
-        neotree_open = true
-      end
-    end)
-  end,
+	config = function()
+		vim.keymap.set("n", "<leader>ee", function()
+			if neotree_open then
+				vim.cmd(":Neotree close")
+				neotree_open = false
+			else
+				vim.cmd(":Neotree filesystem reveal left")
+				neotree_open = true
+			end
+		end)
+	end,
 }
