fe50e06e931c6664434a0bc61d74af55f2a291a9
c3smonkey <c3smonkey@gmail.com>
2024-03-23 20:29:04 +0100

    chore: [nvim] modular setup add lsp configuration

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-lualine/lualine.nvim",
-
   config = function()
     require('lualine').setup({
       option = {
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,7 +1,28 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.g.netrw_banner = 0
+
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"
+
+opt.spelllang = "en_us,de,de_ch"
+opt.spell = true
+opt.swapfile = false
+opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 2
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+opt.scrolloff = 10
+
 
 
