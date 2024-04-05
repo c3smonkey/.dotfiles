e30201543bdda701f3b4213c7173df0cfb40620a
c3smonkey <c3smonkey@gmail.com>
2024-04-05 14:58:07 +0200

    chore: [nvim] configure luanine theme dracula

--- .config/nvim/lua/c3s/plugins/lualine.lua
+++ .config/nvim/lua/c3s/plugins/lualine.lua
@@ -1,11 +1,10 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    require('lualine').setup({
+    require("lualine").setup({
       option = {
-        --theme = 'dracula'
-      }
+        theme = "dracula",
+      },
     })
-  end
+  end,
 }
-
