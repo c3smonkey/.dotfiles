3db8d466800ccb4a8a883f7ee7f0f64047bba994
c3smonkey <c3smonkey@gmail.com>
2024-04-05 21:56:22 +0200

    chore: [nvim] change autocompletion (CMP)

--- .config/nvim/lua/c3s/plugins/git.lua
+++ .config/nvim/lua/c3s/plugins/git.lua
@@ -38,20 +38,4 @@ return {
       )
     end,
   },
-  -- {
-  -- 	"lewis6991/gitsigns.nvim",
-  -- 	event = { "BufReadPre", "BufNewFile" },
-  -- 	config = function()
-  -- 		require("gitsigns").setup()
-  --
-  -- 		local keymap = vim.keymap
-  -- 		keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { silent = true, desc = "Git preview hunk" })
-  -- 		keymap.set(
-  -- 			"n",
-  -- 			"<leader>gt",
-  -- 			":Gitsigns toggle_current_line_blame<CR>",
-  -- 			{ silent = true, desc = "Git toggle current line blame" }
-  -- 		)
-  -- 	end,
-  -- },
 }
