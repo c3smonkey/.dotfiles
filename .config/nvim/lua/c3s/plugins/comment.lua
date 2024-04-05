bbda9718b89a0ca883efc464d83d17dae9381789
c3smonkey <c3smonkey@gmail.com>
2024-04-05 21:23:55 +0200

    chore: [nvim] add trouble plugin

--- .config/nvim/lua/c3s/plugins/comment.lua
+++ .config/nvim/lua/c3s/plugins/comment.lua
@@ -1,21 +1,17 @@
 return {
-    {
-        "numToStr/Comment.nvim", opts = {}
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+  },
+  -- Highlight todo, notes, etc in comments
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
-    -- Highlight todo, notes, etc in comments
-    -- WARN: to enable it wirte WARN:
-    -- INFO: to enable it write INFO:
-    -- NOTE: to enable it write NOTE:
-    -- INFO: to enable it write INFO:
-    -- FIXME: to enable it wirte FIXME:
-    {
-        "folke/todo-comments.nvim",
-        event = "VimEnter",
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        },
-        opts = {
-            signs = false
-        }
+    opts = {
+      signs = false,
     },
+  },
 }
