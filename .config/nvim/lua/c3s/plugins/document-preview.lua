57583703d8e4a9d74fedfba375161df59513623b
c3smonkey <c3smonkey@gmail.com>
2024-04-14 20:51:11 +0200

    chore: [nvim] Asciidoc Preview [ap]

--- .config/nvim/lua/c3s/plugins/document-preview.lua
+++ .config/nvim/lua/c3s/plugins/document-preview.lua
@@ -1,5 +1,15 @@
 return {
 	{
+		'tigion/nvim-asciidoc-preview',
+		cmd = { 'AsciiDocPreview' },
+		ft = { 'asciidoc' },
+		build = 'cd server && npm install',
+		opts = {
+			-- Add user configuration here
+		},
+		vim.keymap.set("n", "<leader>ap", vim.cmd.AsciiDocPreview, { desc = "[A]sciiDoc [P]review" }),
+	},
+	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
@@ -7,4 +17,5 @@ return {
 		vim.keymap.set("n", "<leader>mps", vim.cmd.MarkdownPreview, { desc = "[M]arkdown [P]review [S]art" }),
 		vim.keymap.set("n", "<leader>mpS", vim.cmd.MarkdownPreviewStop, { desc = "[M]arkdown [P]review [S]top" })		
 	}
+
 }
