2873de0df547dd97c96e4e28c390aa3cac380401
Marcel Widmer <marcel.widmer@helsana.ch>
2024-04-15 20:24:09 +0200

    kevinfengcs88

---
+++ .config/nvim-kevinfengcs88/README.md
@@ -0,0 +1,14 @@
+# My Neovim Config
+This repository contains my Neovim config, which is written in Lua. It uses [lazy.nvim](https://github.com/folke/lazy.nvim) for managing plugins.
+
+## work in progress...
+
+empty...
+
+## Uninstalling
+
+I build Neovim from source. To uninstall just run:
+
+```
+rm -f $(which nvim)
+```
---
+++ .config/nvim-kevinfengcs88/init.lua
@@ -0,0 +1,21 @@
+--vim.g.clipboard = {
+--    name = 'WslClipboard',
+--    copy = {
+--        ['+'] = 'clip.exe',
+--        ['*'] = 'clip.exe',
+--    },
+--    paste = {
+--        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring():gsub("\\r", ""))',
+--        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring():gsub("\\r", ""))',
+--    },
+--    cache_enabled = 0,
+--}
+
+require('v9.options')
+require('v9.keymaps')
+require('v9.lazy')
+require('v9.colorscheme')
+vim.opt.guicursor = ''
+
+vim.cmd([[autocmd FileType yaml setlocal indentexpr=]])
+vim.cmd([[autocmd FileType yml setlocal indentexpr=]])
