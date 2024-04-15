35a93d00f22d3a2fcdba26c82673b0b1105f6d02
Marcel Widmer <marcel.widmer@helsana.ch>
2024-04-15 18:23:02 +0200

    init

---
+++ .config/nvim-me/init.lua
@@ -0,0 +1,11 @@
+-- init.lua
+-- load settings before lazy
+require("config.settings")
+require("config.keymaps")
+require("config.lazy")
+
+-- set colorscheme
+-- INFO: colorscheme must be set after lazy loading, otherwise it will be overwritten
+-- normally it should be set in the plugin colorscheme. Sometinge it is not working so it is set here
+vim.cmd("colorscheme catppuccin-mocha")
+-- vim.cmd("colorscheme monokai-pro-spectrum")
