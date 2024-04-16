0f0681aab761d06a6b341397b64e313703eb5fc4
Marcel Widmer <marcel.widmer@helsana.ch>
2024-04-16 21:25:49 +0200

    add gruvebox

--- .config/nvim/lua/pluginlist.lua
+++ .config/nvim/lua/pluginlist.lua
@@ -1,10 +1,17 @@
 -- nvim/lua/pluginlist.lua
 return {
-				{
-						'numToStr/Comment.nvim',
-						opts = {
-								-- add any options here
-						},
-						lazy = false,
-				}
+		{
+				'numToStr/Comment.nvim',
+				opts = {
+						-- add any options here
+				},
+				lazy = false,
+		},
+		{ 
+				"ellisonleao/gruvbox.nvim", 
+				priority = 1000 ,
+				config = function()
+						vim.cmd("colorscheme gruvbox")
+				end
+		}
 }
