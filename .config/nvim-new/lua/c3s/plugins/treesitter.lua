f6e23e015529b0424ea8d8d46a3b2274b2cbe66e
Marcel Widmer <marcel.widmer@helsana.ch>
2024-04-16 06:27:17 +0200

    kevinfengcs88

---
+++ .config/nvim-new/lua/c3s/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        event = { 'BufReadPre', 'BufNewFile' },
+        cmd = { 'TSInstallInfo', 'TSInstall' },
+        config = function()
+            local status_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
+
+            if not status_ok then
+                return
+            end
+
+            treesitter.setup({
+                modules = {},
+                ensure_installed = { 'lua', 'vim', 'kotlin', 'markdown_inline', 'go' },
+                highlight = {
+                    enable = true
+                },
+                sync_install = false,
+                auto_install = false,
+                ignore_install = {},
+                context_commentstring = {
+                    enable = true,
+                    autocmd = false
+                },
+                autopairs = {
+                    enable = true
+                },
+                autotag = {
+                    enable = true
+                },
+                indent = {
+                    enable = true,
+                    disable = { 'go' }
+                }
+            })
+        end
+
+    }
+}
