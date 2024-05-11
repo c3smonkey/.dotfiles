9a478f6ad0b1f85c0fb07320821ae6a91b4d440e
monkey <marzelwidmer@gmail.com>
2024-05-12 00:36:16 +0200

    chore: [sesh] update some configuration

--- .config/sesh/startup.sh
+++ .config/sesh/startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 tmux split-window -v
 tmux select-pane -t :.+
-tmux send-keys "lt2" Enter
+tmux send-keys "vi +Explor" Enter
 tmux select-pane -t 1
-tmux send-keys "git status" Enter
+tmux send-keys "lt2" Enter
