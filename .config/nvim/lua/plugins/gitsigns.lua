7d76554836aba30b3a0100193bbae8494a541d82
monkey <marzelwidmer@gmail.com>
2024-05-11 21:19:43 +0200

    chore: [nvim] change style of gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -14,12 +14,19 @@ return {
         else
             --TODO: configurable via customizer
             opts.signs = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                changedelete = { text = "▎" },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                untracked = { text = "▎" },
+                --[[ add = { text = "▎" },
+                 change = { text = "▎" },
+                 changedelete = { text = "▎" },
+                 delete = { text = '_' },
+                 topdelete = { text = '‾' },
+                 untracked = { text = "▎" },]]
+
+                add = { text = "" },
+                change = { text = "" },
+                delete = { text = "" },
+                topdelete = { text = "" },
+                changedelete = { text = "" },
+                untracked = { text = "" },
             }
             opts.current_line_blame = true
             opts.current_line_blame_formatter = " <author>, <author_time:%Y-%m-%d> - <summary>"
@@ -33,82 +40,39 @@ return {
                         vim.keymap.set(mode, lhs, rhs, { buffer = buffer, desc = desc })
                     end
 
-                    map("n", "]h", gs.next_hunk, "Next hunk")
-                    map("n", "[h", gs.prev_hunk, "Prev hunk")
+                    local function map(mode, l, r, desc)
+                        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+                    end
 
-                    map("n", "<leader>gB", function()
+                    map({ "n", "v" }, "<leader>gx", ":Gitsigns reset_hunk<CR>", "[g]it Reset Hunk")
+                    map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "[g]it [s]tage Hunk")
+                    map("n", "<leader>gS", gs.stage_buffer, "[g]it [S]tage Buffer")
+                    map("n", "<leader>gh", gs.preview_hunk, "[g]it Preview [h]unk")
+                    map("n", "<leader>gu", gs.undo_stage_hunk, "[g]it [u]ndo Stage Hunk")
+                    map("n", "<leader>gX", gs.reset_buffer, "[g]it Reset Buffer")
+                    map("n", "<leader>gp", gs.preview_hunk, "[g]it [p]review Hunk")
+
+                    map("n", "<leader>gb", function()
                         gs.blame_line({ full = true })
-                    end, "[g]it [B]lame line full")
-                    map("n", "<leader>gbl", gs.blame_line, "[g]it [b]lame [l]ine")
+                    end, "[g]it [b]lame Line")
 
-                    map({ "n", "v" }, "<leader>gR", gs.reset_hunk, "[g]it [R]eset hunk")
-                    map({ "n", "v" }, "<leader>gh", gs.stage_hunk, "[g]it stage [h]unk")
-                    map("n", "<leader>gH", gs.undo_stage_hunk, "[g]it undo stage [H]unk")
+                    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
 
-                    map("n", "<leader>gp", gs.preview_hunk, "[g]it [p]review hunk")
-                    map("n", "<leader>gr", gs.reset_buffer, "[g]it [r]eset buffer")
-
-                    map({"x", "o"}, "ih", "<cmd><C-U>Gitsigns select_hunk<cr>", "Gitsigns Select Hunk")
-
-                    map("n", "<leader>gt","<cmd>Gitsigns toggle_current_line_blame<cr>", "[g]it [t]oggle blame line")
-
+                    map("n", "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<cr>", "[g]it [t]oggle blame line")
 
                 end
             end
         end
     end,
+    keys = {
+        -- git hunk navigation - previous / next
+        { "gh", ":Gitsigns next_hunk<CR>", desc = "Goto next git hunk" },
+        { "gH", ":Gitsigns prev_hunk<CR>", desc = "Goto previous git hunk" },
+        { "]g", ":Gitsigns next_hunk<CR>", desc = "Goto next git hunk" },
+        { "[g", ":Gitsigns prev_hunk<CR>", desc = "Goto previous git hunk" },
+    },
 
     config = function(_, opts)
         require('gitsigns').setup(opts)
     end,
 }
-
-
-
--- from josh
--- cSpell:words gitsigns nvim topdelete changedelete keymap stylua diffthis
---return {
---    "lewis6991/gitsigns.nvim",
---    event = "BufReadPre",
---    opts = function()
---        -- local icons = require("config.icons")
---        --- @type Gitsigns.Config
---        local C = {
---            signs = {
---                add = { text = "" },
---                change = { text = "" },
---                delete = { text = "" },
---                topdelete = { text = "" },
---                changedelete = { text = "" },
---                untracked = { text = "" },
---            },
---            on_attach = function(buffer)
---                local gs = package.loaded.gitsigns
---
---                local function map(mode, l, r, desc)
---                    vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
---                end
---
---                map({ "n", "v" }, "<leader>gx", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
---                map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
---                map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
---                map("n", "<leader>gh", gs.preview_hunk, "Preview Hunk")
---                map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
---                map("n", "<leader>gX", gs.reset_buffer, "Reset Buffer")
---                map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
---                map("n", "<leader>gb", function()
---                    gs.blame_line({ full = true })
---                end, "Blame Line")
---                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
---            end,
---        }
---        return C
---    end,
---    keys = {
---        -- git hunk navigation - previous / next
---        { "gh", ":Gitsigns next_hunk<CR>", desc = "Goto next git hunk" },
---        { "gH", ":Gitsigns prev_hunk<CR>", desc = "Goto previous git hunk" },
---        { "]g", ":Gitsigns next_hunk<CR>", desc = "Goto next git hunk" },
---        { "[g", ":Gitsigns prev_hunk<CR>", desc = "Goto previous git hunk" },
---    },
---}
