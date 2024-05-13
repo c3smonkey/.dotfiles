25c94686e27443eed4f20a62f30366eb9570fb7b
monkey <marzelwidmer@gmail.com>
2024-05-13 23:03:28 +0200

    fix: [nvim] keymap which-key

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -19,7 +19,7 @@ return {
             Keymap('n', '<leader>hm', "<CMD>lua require('harpoon.mark').add_file()<CR>", { desc = '[h]arpoon [m]ark' })
             Keymap('n', '<leader>hn', "<CMD>lua require('harpoon.ui').nav_next()<CR>", { desc = '[h]arpoon [n]ext Mark' })
             Keymap('n', '<leader>hp', "<CMD>lua require('harpoon.ui').nav_prev()<CR>", { desc = '[h]arpoon [p]revious Mark' })
-            Keymap('n', '<leader>ht', "<CMD>lua require('harpoon.term').gotoTerminal(1)<CR>", { desc = '[h]arpoON [t]erminal' })
+            Keymap('n', '<leader>ht', "<CMD>lua require('harpoon.term').gotoTerminal(1)<CR>", { desc = '[h]arpoon [t]erminal' })
         end
     end,
 
