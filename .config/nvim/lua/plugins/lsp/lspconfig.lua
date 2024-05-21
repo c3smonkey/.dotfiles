e7c9b71583f8f868797e0208ae4bb28b3ea0cc3a
monkey <marzelwidmer@gmail.com>
2024-05-21 07:52:54 +0200

    fix: [nvim] which key for LSP

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -23,17 +23,17 @@ return {
                     vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
                 end
 
-                map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-                map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+                map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [d]efinition")
+                map("gr", require("telescope.builtin").lsp_references, "[G]oto [r]eferences")
                 map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
                 map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-                map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-                map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-                map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-                map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+                map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[d]ocument [s]ymbols")
+                map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[w]orkspace [s]ymbols")
+                map("<leader>rn", vim.lsp.buf.rename, "[r]e[n]ame")
+                map("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
                 map("K", vim.lsp.buf.hover, "Hover Documentation")
-                map("<leader>rs", ":LspRestart<CR>", "[R]estart L[S]P")
-                map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+                map("<leader>rs", ":LspRestart<CR>", "[r]estart L[s]P")
+                map("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
 
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
                 if client and client.server_capabilities.documentHighlightProvider then
