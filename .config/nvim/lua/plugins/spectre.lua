a4c1a474d1963525ac7a75369f0196ce8ef7f3d2
monkey <marzelwidmer@gmail.com>
2024-05-23 08:47:39 +0200

    fix: [nvim] spectre theme, remove own theme

--- .config/nvim/lua/plugins/spectre.lua
+++ .config/nvim/lua/plugins/spectre.lua
@@ -14,10 +14,6 @@ return {
 			"catppuccin/nvim",
 		},
 		config = function()
-			local theme = require("catppuccin.palettes").get_palette("macchiato")
-
-			vim.api.nvim_set_hl(0, "SpectreSearch", { bg = theme.red, fg = theme.base })
-			vim.api.nvim_set_hl(0, "SpectreReplace", { bg = theme.green, fg = theme.base })
 
 			require("spectre").setup({
 				highlight = {
