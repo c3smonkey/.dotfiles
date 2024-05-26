b1b28aba1071dc9866eb7a92d6d11bcfb23f2150
monkey <marzelwidmer@gmail.com>
2024-05-26 10:27:12 +0200

    fix: [nvim] remove luasnip from autocomplete.lua

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -67,31 +67,4 @@ return {
             })
         end
     },
-    {
-        "L3MON4D3/LuaSnip",
-        event = "InsertEnter",
-        version = "v2.*",
-        build = "make install_jsregexp",
-        dependencies = { "saadparwaiz1/cmp_luasnip" },
-        keys = {
-            {
-                "<c-l>",
-                function() require("luasnip").jump(1) end,
-                desc = "Jump forward a snippet placement",
-                mode = "i",
-                noremap = true,
-                silent = true
-            }, {
-                "<c-h>",
-                function() require("luasnip").jump(-1) end,
-                desc = "Jump backward a snippet placement",
-                mode = "i",
-                noremap = true,
-                silent = true
-            }
-        },
-        config = function()
-            require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/snippets"})
-         end
-    }
-}
+  }
