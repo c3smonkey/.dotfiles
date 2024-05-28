c7cc9d1498f3a7125ff45af330f5dc6f67e7f3de
Marcel Widmer <marcel.widmer@helsana.ch>
2024-05-28 17:13:52 +0200

    chore: [zsh] add alias list port and change notes.zsh repo path

--- zsh/plugins/custom/note.zsh
+++ zsh/plugins/custom/note.zsh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-ZET="$HOME/git/zettelkasten/$(date +%Y-%m-%d).md"
+# ZET="$HOME/git/zettelkasten/$(date +%Y-%m-%d).md"
+ZET="$HOME/git/github/marzelwidmer/zettelkasten/$(date +%Y-%m-%d).md"
+
 
 note() {
     echo "# $(date)" >> $ZET
