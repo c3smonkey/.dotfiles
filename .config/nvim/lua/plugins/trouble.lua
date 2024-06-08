0243138298ae5f035af0064cae93ecf2265c7066
monkey <marzelwidmer@gmail.com>
2024-06-08 20:48:03 +0200

    chore: [nvim] update trouble plugin

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -3,16 +3,44 @@ require("config.c3s.globals")
 local plugin = "trouble.nvim"
 
 return {
-  "folke/" .. plugin,
-  event = { "BufReadPost", "BufNewFile" },
-  enabled = Is_Enabled(plugin),
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-  keys = {
-    { "<leader>tt", "<CMD>TroubleToggle<CR>", desc = "[t] [t]rouble List" },
-    { "<leader>tw", "<CMD>TroubleToggle workspace_diagnostics<CR>", desc = "[t]rouble [w]orkspace Diagnostics" },
-    { "<leader>td", "<CMD>TroubleToggle document_diagnostics<CR>", desc = "[t]rouble [d]ocument Diagnostics" },
-    { "<leader>tq", "<CMD>TroubleToggle quickfix<CR>", desc = "[t]rouble [q]uickfix List" },
-    { "<leader>tl", "<CMD>TroubleToggle loclist<CR>", desc = "[t]rouble [l]ocation List" },
-    { "<leader>to", "<CMD>TodoTrouble<CR>", desc = "[t]rouble t[o]dos" },
-  },
+	{
+		"folke/" .. plugin,
+		enabled = Is_Enabled(plugin),
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>tt",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "[t]roube diagnostics [t]oggle",
+			},
+			{
+				"<leader>tb",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "[t]rouble [b]uffer Diagnostics",
+			},
+			{
+				"<leader>ts",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "[t]rouble [s]ymbols",
+			},
+			{
+				"<leader>tl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "[t]rouble [l]sp",
+			},
+			{
+				"<leader>tL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "[t]roube [L]ocation list",
+			},
+			{
+				"<leader>tQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "[t]rouble [Q]uickfix list",
+			},
+		},
+	}
 }
