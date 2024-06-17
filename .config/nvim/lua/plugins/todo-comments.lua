988160861f2013bec57d06710b5753c53647c54a
monkey <marzelwidmer@gmail.com>
2024-06-17 18:26:26 +0200

    chore: [nvim] change color of todo-comments plugin

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -3,22 +3,22 @@ require("config.c3s.globals")
 local plugin = "todo-comments.nvim"
 
 return {
-    "folke/" .. plugin,
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    enabled = Is_Enabled(plugin),
-    opts = function(_, opts)
-        if Use_Default_Opts(plugin) then
-            opts = opts
-        else
-            opts.signs = true
-            opts.merge_keywords = true
-            opts.keywords = {
-                NOTE = { icon = " ", color = "#8A69BB", alt = {"note"}},
-                INFO = { icon = " ", color = "#ffd966", alt = {"info"}},
-            }
-        end
-    end,
+	"folke/" .. plugin,
+	event = { "BufReadPost", "BufNewFile" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	enabled = Is_Enabled(plugin),
+	opts = function(_, opts)
+		if Use_Default_Opts(plugin) then
+			opts = opts
+		else
+			opts.signs = true
+			opts.merge_keywords = true
+			opts.keywords = {
+				NOTE = { color = "#8A69BB", alt = {"note"}},
+				INFO = { icon = " ", color = "#ffd966", alt = {"info"}},
+			}
+		end
+	end,
 }
