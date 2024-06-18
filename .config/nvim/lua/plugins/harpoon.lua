8fc8568d73655a53834d205e724544304d610647
monkey <marzelwidmer@gmail.com>
2024-06-18 18:10:29 +0200

    chore: [ideavim] configure harpoon

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -17,8 +17,10 @@ return {
         end
         if not Use_Default_Keys(plugin) then
             Keymap('n', '<leader>hm', "<CMD>lua require('harpoon.mark').add_file()<CR>", { desc = '[h]arpoon [m]ark' })
-            Keymap('n', '<leader>hn', "<CMD>lua require('harpoon.ui').nav_next()<CR>", { desc = '[h]arpoon [n]ext Mark' })
-            Keymap('n', '<leader>hp', "<CMD>lua require('harpoon.ui').nav_prev()<CR>", { desc = '[h]arpoon [p]revious Mark' })
+            Keymap('n', '<C-A-j>', "<CMD>lua require('harpoon.ui').nav_next()<CR>", { desc = '[h]arpoon [n]ext Mark' })
+            Keymap('n', '<C-A-k>', "<CMD>lua require('harpoon.ui').nav_prev()<CR>", { desc = '[h]arpoon [p]revious Mark' })
+            -- Keymap('n', '<leader>hn', "<CMD>lua require('harpoon.ui').nav_next()<CR>", { desc = '[h]arpoon [n]ext Mark' })
+            -- Keymap('n', '<leader>hp', "<CMD>lua require('harpoon.ui').nav_prev()<CR>", { desc = '[h]arpoon [p]revious Mark' })
             Keymap('n', '<leader>hq', "<CMD>lua require('harpoon.ui').toggle_quick_menu()<CR>", { desc = '[h]arpoon [q]uick menu' })
             Keymap('n', '<leader>ht', "<CMD>lua require('harpoon.term').gotoTerminal(1)<CR>", { desc = '[h]arpoon [t]erminal' })
         end
