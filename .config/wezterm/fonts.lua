d469d787f3c7dbfba6c04e812db779d0afd44b05
monkey <marzelwidmer@gmail.com>
2024-07-05 20:32:03 +0200

    chore: [wezterm] change font

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -2,17 +2,21 @@ local wezterm = require('wezterm')
 --
 local fonts = {}
 
-font_size = 13.0
-line_height = 1.2
-weight = "Regular"
+font_size = 11.0
+line_height = 1.1
+weight = "Light"
+
 fontName = "JetBrainsMono Nerd Font Mono"
--- fontName = "JetBrains Mono"
---fontName = "FiraCode Nerd Font Mono"
---fontName = "MesloLGS NF"
---fontName = "ComicShannsMono"
---fontName = "CommitMono Nerd Font"
---fontName = "Geist-Thin"
---fontName = "Monoid Nerd Font Mono"
+fontName = "JetBrains Mono"
+fontName = "FiraCode Nerd Font Mono"
+fontName = "MesloLGS NF"
+fontName = "ComicShannsMono"
+fontName = "CommitMono Nerd Font"
+fontName = "Monoid Nerd Font Mono"
+fontName = "Geist-Thin"
+
+
+fontName = "Fira Code"
 
 function fonts.setup(config)
     config.font = wezterm.font(fontName, { weight = weight })
