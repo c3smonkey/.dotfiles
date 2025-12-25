a3ba6ae90297ffc4ce0cd68f0c762d53af73b7b6
marzelwidmer <marzelwidmer@example.com>
2025-12-25 09:42:02 +0100

    chore: [zshrc,wezterm] add fix repo. ssh and background werzterm

--- zsh/plugins/custom/gitUser.zsh
+++ zsh/plugins/custom/gitUser.zsh
@@ -19,3 +19,25 @@ gitsetmarzel() {
   git config user.email "marzelwidmer@gmail.com" # <-- hier ggf. echte E-Mail
   echo "Git user set to 'marzelwidmer <marzelwidmer@gmail.com>' for this repository."
 }
+
+# Git Fix
+gitfixmarzel() {
+  git rev-parse --is-inside-work-tree >/dev/null 2>&1 || {
+    echo "Not inside a git repository!"
+    return 1
+  }
+
+  old=$(git remote get-url origin)
+
+  if [[ "$old" != git@github.com:* ]]; then
+    echo "Remote is not github.com, nothing to fix."
+    return 0
+  fi
+
+  new=$(echo "$old" | sed 's|git@github.com:|git@github-marzel:|')
+  git remote set-url origin "$new"
+
+  echo "✔ Remote fixed:"
+  echo "  $old"
+  echo "  → $new"
+}
