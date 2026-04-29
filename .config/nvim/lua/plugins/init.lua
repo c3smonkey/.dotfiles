8fa173167ba47530be6c0a8586b5151b0a8561c3
Marcel Widmer <marcel.widmer@helsana.ch>
2026-04-29 14:56:28 +0200

    chore: add lsp

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,8 +1,17 @@
 vim.pack.add({
-  'https://github.com/folke/which-key.nvim',
-  'https://github.com/neovim/nvim-lspconfig',
+  -- LSP (using native Neovim 0.11+ vim.lsp.config)
+  'https://github.com/williamboman/mason.nvim',
+  
+  -- DAP (Debugging)
+  'https://github.com/mfussenegger/nvim-dap',
+  'https://github.com/rcarriga/nvim-dap-ui',
+  'https://github.com/nvim-neotest/nvim-nio',
+  
+  -- Utilities
+  'https://github.com/nvim-mini/mini.nvim',
   'https://github.com/nvim-lua/plenary.nvim',
   'https://github.com/christoomey/vim-tmux-navigator',
+  'https://github.com/folke/which-key.nvim',
   'https://github.com/ibhagwan/fzf-lua',
   'https://github.com/kdheepak/lazygit.nvim',
 })
@@ -11,4 +20,7 @@ vim.pack.add({
 -- │  Konfigurationen     │
 -- ╰──────────────────────╯
 require("plugins.whichkey")
-require("plugins.fzflua")
+require("plugins.fzf")
+require("plugins.mini")
+require("plugins.lsp")
+require("plugins.dap")
